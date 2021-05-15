43abec4e56ba2f50fe546ab848d6399fad8a20aa
MMORONTO <t2wycemo@gmail.com>
2021-05-15 04:24:54 -0400

    add view extent to base map

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function init(){
     // ##Specify view##
     const map = new ol.Map({
         view: new ol.View({
-            center: [15091875.539375868, -2890099.0297847847],
-            zoom: 1,
-            extent: [11644482.371265175, -5927677.981920381, 17897308.66780227, 423055.837164]
+            center: [-110, 40],
+            zoom: 0.50,
+            extent: [-2928417.2600459913, -5356355.993575926, 6819329.638905743, 5231577.155380443],
         }),
         layers: [
             new ol.layer.Tile({
@@ -14,5 +14,9 @@ function init(){
             })
         ],
         target: 'openlayers-map'
+    })
+
+    map.on('click', function(e){
+        console.log(e.coordinate);
     })
 }
